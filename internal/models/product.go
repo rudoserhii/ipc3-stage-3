7f3018dd2682982d0dcf4459b5721b4cb0de69c7
rudoserhii <rudoserhii@gmail.com>
2024-06-27 00:58:20 +0100

    feat: implement product model

--- internal/models/product.go
+++ internal/models/product.go
@@ -5,21 +5,38 @@ import "github.com/obiMadu/ipc3-stage-3/internal/interfaces"
 type ProductModel struct{}
 
 func (p *ProductModel) GetAllProducts() ([]interfaces.Product, error) {
-	return []interfaces.Product{}, nil
+	var products []interfaces.Product
+	if err := DB.Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
 }
 
 func (p *ProductModel) GetProductByID(productID uint) (*interfaces.Product, error) {
-	return &interfaces.Product{}, nil
+	var product interfaces.Product
+	if err := DB.First(&interfaces.Product{}, product).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
 }
 
-func (p *ProductModel) CreateProduct(product *interfaces.Product) error {
+func (p *ProductModel) CreateProduct(product interfaces.Product) error {
+	if err := DB.Create(&product).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func (p *ProductModel) UpdateProductByID(product *interfaces.Product) error {
+func (p *ProductModel) UpdateProductByID(product interfaces.Product) error {
+	if err := DB.Save(&product).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
 func (p *ProductModel) DeleteProductByID(productID uint) error {
+	if err := DB.Delete(&interfaces.Product{}, productID).Error; err != nil {
+		return err
+	}
 	return nil
 }
