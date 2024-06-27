9030e2bb320a1012bdc9cacde003ff3a39f22efd
rudoserhii <rudoserhii@gmail.com>
2024-06-27 06:43:31 +0100

    feat: remove un-useful model field

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,14 +3,15 @@ package handlers
 import "github.com/obiMadu/ipc3-stage-3/internal/interfaces"
 
 type Handlers struct {
-	Models  interfaces.Models
 	product ProductRoutes
 	order   OrderRoutes
 }
 
+var Models interfaces.Models
+
 func NewHandlers(models interfaces.Models) *Handlers {
+	Models = models
 	return &Handlers{
-		Models:  models,
 		product: ProductRoutes{},
 		order:   OrderRoutes{},
 	}
